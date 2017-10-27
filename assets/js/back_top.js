8464a888693a912af27f5b78458d61093e554213
nhoxzunbmt <nhoxzunbmt@users.noreply.github.com>
2017-10-27 17:00:29 +0700

    First

---
+++ assets/js/back_top.js
@@ -0,0 +1,14 @@
+$('#backtotop').click(function (e) {
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: 0
+    }, 500);
+});
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 60) {
+        $('.back-to-top').slideDown();
+    } else {
+        $('.back-to-top').slideUp();
+    }
+});
---
+++ webpack.mix.js
@@ -0,0 +1,26 @@
+let mix = require('laravel-mix');
+mix.setPublicPath('app');
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+
+   npm run dev
+   npm run production
+   npm run watch
+   npm run watch-poll
+   taskkill /f /im node.exe
+
+ */
+mix.options({processCssUrls: false});
+
+
+mix.sass('assets/sass/app.scss', 'app/css');
+mix.js('assets/js/app.js', 'app/js');
+mix.copy('node_modules/bootstrap-sass/assets/fonts', 'app/fonts', false);
+// mix.copy('assets/images', 'app/images', false);
+mix.copy('assets/fonts', 'app/fonts', false);
